9ef1a06f83a9e3f4c7321a739e5876015cbbbb98
Vitor Ferraz <vitorferrazmaciel@gmail.com>
2022-05-17 14:27:13 -0300

    solid more

--- src/routes/categories.routes.ts.ts
+++ src/routes/categories.routes.ts.ts
@@ -1,18 +1,24 @@
 import { Router, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 const categoriesRoutes = Router();
 
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body;
-  const category = {
-    name,
-    description,
-    id: uuidv4(),
-  };
-  categories.push(category);
-  return response.status(201).send();
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).send("Category successfully created");
+});
+
+categoriesRoutes.get("/", (request: Request, response: Response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
